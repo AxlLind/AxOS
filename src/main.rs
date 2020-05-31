2d29aa58bebbaa4bde8f5d1cd9fa6466c7c03600
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-05-31 11:22:57 +0200

    First running OS!
    This is a very stupid OS, it just enters an infinite loop. But now all
    toolchains are up and running. Can now build and run an OS image!
    
    Commands to run:
    cargo bootimage --target=this-os.json
    qemu-system-x86_64 -drive format=raw,file=target/this-os/debug/bootimage-this_os.bin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(core_intrinsics)]
+#![no_std]
+#![no_main]
+
+extern crate bootloader;
+
+use core::intrinsics;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+fn panic(_info: &PanicInfo) -> ! {
+  intrinsics::abort()
+}
+
+#[no_mangle]
+pub fn _start() -> ! {
+    loop {}
 }
