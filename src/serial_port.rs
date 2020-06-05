8471c6ce86687fde4115efdf94f2d74716be7d7c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-06-05 17:39:03 +0200

    SerialPort: No longer initialize on creation
    This commit changes the SerialPort to no longer initialize on creation.
    Instead you now create a SerialPort via `SerialPort(PORT)` and call
    `SerialPort::initialize(PORT)` separately. This makes static SerialPort
    variables much simpler.

--- src/serial_port.rs
+++ src/serial_port.rs
@@ -1,12 +1,7 @@
 #![allow(unused)]
 
 /*
-  This is a low level abstraction of a serial port.
-  Initializes the port on creation. It does not
-  check that this port has already been initialized,
-  that is up to the creator of the SerialPort struct!
-
-  References:
+  A low level abstraction of a serial port. References:
   https://wiki.osdev.org/Serial_Ports
   https://c9x.me/x86/html/file_module_x86_id_222.html
   https://c9x.me/x86/html/file_module_x86_id_139.html
@@ -53,10 +48,11 @@ impl SerialPortType for u32 {
   }
 }
 
-pub struct SerialPort(u16);
+#[derive(Clone,Copy)]
+pub struct SerialPort(pub u16);
 
 impl SerialPort {
-  pub fn initialize(port: u16) -> Self {
+  pub fn initialize(port: u16) {
     u8::serial_send(port + 1, 0x00); // Disable all interrupts
     u8::serial_send(port + 3, 0x80); // Enable DLAB (set baud rate divisor)
     u8::serial_send(port + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
@@ -64,7 +60,6 @@ impl SerialPort {
     u8::serial_send(port + 3, 0x03); // 8 bits, no parity, one stop bit
     u8::serial_send(port + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
     u8::serial_send(port + 4, 0x0B); // IRQs enabled, RTS/DSR set
-    Self(port)
   }
 
   pub fn read<T: SerialPortType>(&self) -> T {
