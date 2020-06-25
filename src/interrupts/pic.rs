c4ae89edea0194a9aa9dbfd50017c36de02b9918
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-06-25 12:21:07 +0200

    IO Port: Refactor io send/read wrappers
    Previously a trait IoPortType was implemented for u8,u16,u32 and the
    methods were generic over these. After some more implementations, only
    the u8 version has been used. Might never need u16 and u32 versions so
    lets make u8 the "default" so we do not have to write `::<u8>`
    everywhere and instead add io_read_u32 functions.

--- src/interrupts/pic.rs
+++ src/interrupts/pic.rs
@@ -1,23 +1,23 @@
-use crate::io_port::IoPortType;
+use crate::io_port::io_send;
 
 const PIC1_CMD: u16 = 0x20; const PIC1_DATA: u16 = 0x21;
 const PIC2_CMD: u16 = 0xa0; const PIC2_DATA: u16 = 0xa1;
 
 pub fn initialize() {
-  u8::io_send(PIC1_CMD,  0x11); // start initialization
-  u8::io_send(PIC2_CMD,  0x11); // ..
-  u8::io_send(PIC1_DATA, 0x20); // specify the vector offset
-  u8::io_send(PIC2_DATA, 0x28); // ..
-  u8::io_send(PIC1_DATA, 0x04); // let the pics know about each other
-  u8::io_send(PIC2_DATA, 0x02); // ..
-  u8::io_send(PIC1_DATA, 0x01); // tell them what type of pic they are
-  u8::io_send(PIC2_DATA, 0x01); // ..
-  u8::io_send(PIC1_DATA, 0xfe); // enable/disable certain interrupts
-  u8::io_send(PIC2_DATA, 0xff); // ..
+  io_send(PIC1_CMD,  0x11); // start initialization
+  io_send(PIC2_CMD,  0x11); // ..
+  io_send(PIC1_DATA, 0x20); // specify the vector offset
+  io_send(PIC2_DATA, 0x28); // ..
+  io_send(PIC1_DATA, 0x04); // let the pics know about each other
+  io_send(PIC2_DATA, 0x02); // ..
+  io_send(PIC1_DATA, 0x01); // tell them what type of pic they are
+  io_send(PIC2_DATA, 0x01); // ..
+  io_send(PIC1_DATA, 0xfe); // enable/disable certain interrupts
+  io_send(PIC2_DATA, 0xff); // ..
 }
 
 // unsafe since irq has to match the current interrupt
 pub unsafe fn end_of_interrupt(irq: u8) {
-  if irq >= 8 { u8::io_send(PIC2_CMD, 0x20); }
-  u8::io_send(PIC1_CMD, 0x20);
+  if irq >= 8 { io_send(PIC2_CMD, 0x20); }
+  io_send(PIC1_CMD, 0x20);
 }
--- src/serial_port.rs
+++ src/serial_port.rs
@@ -1,5 +1,5 @@
 #![allow(unused)]
-use crate::io_port::IoPortType;
+use crate::io_port::{io_send, io_read};
 
 /*
   Low level helpers for reading/writing
@@ -10,21 +10,21 @@ use crate::io_port::IoPortType;
 */
 
 pub fn initialize(port: u16) {
-  u8::io_send(port + 1, 0x00); // Disable all interrupts
-  u8::io_send(port + 3, 0x80); // Enable DLAB (set baud rate divisor)
-  u8::io_send(port + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
-  u8::io_send(port + 1, 0x00); //                  (hi byte)
-  u8::io_send(port + 3, 0x03); // 8 bits, no parity, one stop bit
-  u8::io_send(port + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
-  u8::io_send(port + 4, 0x0B); // IRQs enabled, RTS/DSR set
+  io_send(port + 1, 0x00); // Disable all interrupts
+  io_send(port + 3, 0x80); // Enable DLAB (set baud rate divisor)
+  io_send(port + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
+  io_send(port + 1, 0x00); //                  (hi byte)
+  io_send(port + 3, 0x03); // 8 bits, no parity, one stop bit
+  io_send(port + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
+  io_send(port + 4, 0x0B); // IRQs enabled, RTS/DSR set
 }
 
-pub fn read<T: IoPortType>(port: u16) -> T {
-  while u8::io_read(port + 5) & 1 == 0 {}
-  T::io_read(port)
+pub fn read(port: u16) -> u8 {
+  while io_read(port + 5) & 1 == 0 {}
+  io_read(port)
 }
 
-pub fn send<T: IoPortType>(port: u16, t: T) {
-  while u8::io_read(port + 5) & 0x20 == 0 {}
-  T::io_send(port, t);
+pub fn send(port: u16, v: u8) {
+  while io_read(port + 5) & 0x20 == 0 {}
+  io_send(port, v);
 }
