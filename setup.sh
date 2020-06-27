3ab48c6f4e361548812275213dbf90135af0de8a
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-06-27 14:49:58 +0200

    Refactor setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,54 +1,52 @@
-#! /bin/bash
+#!/bin/sh
+
+function fail {
+  echo "Installing $1 failed!"
+  exit 1
+}
 
 # use the custom githooks
 git config core.hooksPath .githooks
 
 # General rust toolchains are needed
 if ! rustup --version > /dev/null 2>&1 ; then
-  curl https://sh.rustup.rs -sSf | sh ||
-    { echo 'Installing rustup failed' ; exit 1; }
+  curl https://sh.rustup.rs -sSf | sh || fail "rustup"
 fi
 
 # Used by some custom scripts
 if ! rg --version > /dev/null 2>&1 ; then
-  cargo install ripgrep ||
-    { echo 'Installing rustup failed' ; exit 1; }
+  cargo install ripgrep || fail "ripgrep"
 fi
 
 # Rust nightly compiler is needed for certain features
 if ! rustup show | grep nightly > /dev/null 2>&1 ; then
-  rustup update nightly ||
-    { echo 'Installing rust nightly failed' ; exit 1; }
+  rustup update nightly || fail "rust nightly"
 fi
 
 # Builds bootable images from rust source
 if ! bootimage --help > /dev/null 2>&1 ; then
-  cargo install bootimage ||
-    { echo 'Installing bootimage failed' ; exit 1; }
+  cargo install bootimage || fail "bootimage"
 fi
 
-# Required to build our on stdlib from source
+# Required to custom build to our target
 if ! cargo xbuild --version > /dev/null 2>&1 ; then
-  cargo install cargo-xbuild ||
-    { echo 'Installing cargo-xbuild failed' ; exit 1; }
+  cargo install cargo-xbuild || fail "cargo-xbuild"
 fi
 
-# Source files for rust core, needed to compile our own stdlib
+# Source files for rust core, needed to compile core to our target
 if ! rustup component list --toolchain=nightly | grep "rust-src.*installed" > /dev/null 2>&1 ; then
-  rustup component add rust-src --toolchain=nightly ||
-    { echo 'Installing rust-src failed' ; exit 1; }
+  rustup component add rust-src --toolchain=nightly || fail "rust-src"
 fi
 
-# Source files for rust core, needed to compile our own stdlib
+# Source files for rust core, needed to compile core to our target
 if ! rustup component list --toolchain=nightly | grep "llvm-tools-preview.*installed" > /dev/null 2>&1 ; then
-  rustup component add llvm-tools-preview --toolchain=nightly ||
-    { echo 'Installing rust-src failed' ; exit 1; }
+  rustup component add llvm-tools-preview --toolchain=nightly || fail "rust-src"
 fi
 
-# QEMU is required to run the OS in a VM
+# Required VM to run the OS in
 if ! qemu-system-x86_64 --version > /dev/null 2>&1 ; then
   echo "Please install qemu: https://www.qemu.org/download/"
   exit 1
 fi
 
-echo "All requirements should be installed!"
+echo "All requirements should now be installed!"
