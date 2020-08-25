25911212f475a6edcf23c7baae8f8e01f6261402
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-08-25 17:09:37 +0200

    allocator: small code cleanup

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,4 +1,4 @@
-use crate::mem::page_table::{page_map_addr, translate_addr};
+use crate::mem::page_table::page_map_addr;
 use crate::mem::VirtAddr;
 use core::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
@@ -14,25 +14,18 @@ struct KernelHeapAllocator {
 }
 
 impl KernelHeapAllocator {
-  fn new() -> Self {
-    Self {
-      ptr: HEAP_START_ADDR,
-    }
-  }
-
   // stupid bump allocator tm
   unsafe fn alloc(&mut self, layout: Layout) -> *mut u8 {
-    let size = layout.size();
-    let align = layout.align();
     if self.ptr + layout.size() > HEAP_END_ADDR {
       return null_mut();
     }
     // align the ptr
+    let align = layout.align();
     if self.ptr & (align - 1) != 0 {
-      self.ptr = ((self.ptr + align - 1) / align) * align;
+      self.ptr = (self.ptr & (align - 1)) + align;
     }
     let res = self.ptr as *mut u8;
-    self.ptr += size;
+    self.ptr += layout.size();
     res
   }
 
@@ -40,7 +33,9 @@ impl KernelHeapAllocator {
 }
 
 lazy_static! {
-  static ref KERNEL_ALLOCATOR: Mutex<KernelHeapAllocator> = Mutex::new(KernelHeapAllocator::new());
+  static ref KERNEL_ALLOCATOR: Mutex<KernelHeapAllocator> = Mutex::new(KernelHeapAllocator {
+    ptr: HEAP_START_ADDR,
+  });
 }
 
 struct AllocatorWrapper;
@@ -59,7 +54,7 @@ static ALLOCATOR: AllocatorWrapper = AllocatorWrapper;
 
 // Called when the kernel fails to allocate memory
 #[alloc_error_handler]
-fn alloc_error(layout: core::alloc::Layout) -> ! {
+fn alloc_error(layout: Layout) -> ! {
   panic!("Kernel OOM, layout {:?}", layout);
 }
 
@@ -68,5 +63,4 @@ pub fn initialize() {
   for page_addr in (HEAP_START_ADDR..HEAP_END_ADDR).step_by(0x1000) {
     page_map_addr(VirtAddr::new(page_addr as u64));
   }
-  assert!(translate_addr(VirtAddr::new(HEAP_START_ADDR as u64)).is_some());
 }
