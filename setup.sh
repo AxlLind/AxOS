366a25d3b9fea6691f668e0bb079aa4994d7d9ed
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-08-26 09:20:48 +0200

    meta: add logging to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,46 +5,56 @@ function fail {
   exit 1
 }
 
-# use the custom githooks
+# Use custom githooks
 git config core.hooksPath .github
+echo "Setting custom githooks path"
 
 # General rust toolchains are needed
-if ! rustup --version > /dev/null 2>&1 ; then
+if rustup --version > /dev/null 2>&1 ; then
+  echo "Already installed: rustup"
+else
   curl https://sh.rustup.rs -sSf | sh || fail "rustup"
 fi
 
-# Used by some custom scripts
-if ! rg --version > /dev/null 2>&1 ; then
-  cargo install ripgrep || fail "ripgrep"
-fi
-
 # Rust nightly compiler is needed for certain features
-if ! rustup show | grep nightly > /dev/null 2>&1 ; then
+if rustup show | grep nightly > /dev/null 2>&1 ; then
+  echo "Already installed: rust nightly"
+else
   rustup update nightly || fail "rust nightly"
 fi
 
 # Builds bootable images from rust source
-if ! bootimage --help > /dev/null 2>&1 ; then
+if bootimage --help > /dev/null 2>&1 ; then
+  echo "Already installed: bootimage"
+else
   cargo install bootimage || fail "bootimage"
 fi
 
-# Required to custom build to our target
-if ! cargo xbuild --version > /dev/null 2>&1 ; then
+# Required to build to our custom target
+if cargo xbuild --version > /dev/null 2>&1 ; then
+  echo "Already installed: cargo-xbuild"
+else
   cargo install cargo-xbuild || fail "cargo-xbuild"
 fi
 
-# Source files for rust core, needed to compile core to our target
-if ! rustup component list --toolchain=nightly | grep "rust-src.*installed" > /dev/null 2>&1 ; then
-  rustup component add rust-src --toolchain=nightly || fail "rust-src"
+# Source files for rust core, needed to compile core to our custom target
+if rustup component list | grep "rust-src.*installed" > /dev/null 2>&1 ; then
+  echo "Already installed: rust-src"
+else
+  rustup component add rust-src || fail "rust-src"
 fi
 
-# Source files for rust core, needed to compile core to our target
-if ! rustup component list --toolchain=nightly | grep "llvm-tools-preview.*installed" > /dev/null 2>&1 ; then
-  rustup component add llvm-tools-preview --toolchain=nightly || fail "rust-src"
+# Needed by bootimage to map our kernel binary
+if rustup component list | grep "llvm-tools-preview.*installed" > /dev/null 2>&1 ; then
+  echo "Already installed: llvm-tools-preview"
+else
+  rustup component add llvm-tools-preview || fail "rust-src"
 fi
 
 # Required VM to run the OS in
-if ! qemu-system-x86_64 --version > /dev/null 2>&1 ; then
+if qemu-system-x86_64 --version > /dev/null 2>&1 ; then
+  echo "Already installed: qemu"
+else
   echo "Please install qemu: https://www.qemu.org/download/"
   exit 1
 fi
